853920b7fb2d66ab826ae4575b2fa5744224dba5
DerekCao <wschy456@163.com>
2017-03-25 13:59:40 +0800

    添加2,3色河内塔解法 重点在: 把河内塔分2层看,需移动到目标柱一层,和不需要移动到目标柱两层,2层 简化为: a层:不需要移动到目标柱的所有层 b层:需要移动到目标柱的层 tag:目标柱 tmp:临时柱 方法M如: 1.a->tmp 2.b->tag 3.tmp->tag 多色解法: 1.保留最下需分开的层,使用方法M移动其他层,到辅助柱 2.利用辅助柱,分开保留层(移动其他层都使用方法M) 3.按色放回起始柱,回到1

--- src/per/chy/hanoi/ctrl/HanoiAnswer.java
+++ src/per/chy/hanoi/ctrl/HanoiAnswer.java
@@ -5,22 +5,26 @@ import per.chy.hanoi.view.GameView;
 public class HanoiAnswer {
 	
 	public interface Answer{
+		//public void moveWidth(int src,int tag);
+		//public void move(int disks,int a,int b,int c);
 		public void work();
 	}
 	Answer as=null;
 	GameView gv=null;
 	int diskn;
 	Answer haniocolor1=new Answer(){
+		public void moveWidth(int src,int tag){
+			gv.moveDisk(src, tag);
+		}
 		public void move(int disks,int a,int b, int c){
 			if(disks==1){
-				gv.moveDisk(a, c);
+				moveWidth(a, c);
 			}else{
 				move(disks-1,a,c,b);
-				gv.moveDisk(a, c);
+				moveWidth(a, c);
 				move(disks-1,b,a,c);
 			}
 		}
-		
 		@Override
 		public void work() {
 			move(diskn,1,2,3);
@@ -30,18 +34,66 @@ public class HanoiAnswer {
 	
 	Answer haniocolor2=new Answer(){
 		
+		public void moveWidth(int src,int tag){
+			gv.moveDisk(src, tag);
+			gv.moveDisk(src, tag);
+		}
+		
+		public void move(int disks,int a,int b,int c){
+			if(disks==2){
+				moveWidth(a,c);
+			}else{
+				move(disks-2,a,c,b);
+				moveWidth(a,c);
+				move(disks-2,b,a,c);
+			}
+		}
+		
 		@Override
 		public void work() {
-			// TODO Auto-generated method stub
+			for(int i=diskn;i>2;i-=2){
+				move(i-2,1,3,2);
+				gv.moveDisk(1, 3);
+				move(i-2,2,1,3);
+				gv.moveDisk(1, 2);
+				move(i-2,3,2,1);
+			}
+			gv.moveDisk(1, 3);
+			gv.moveDisk(1, 2);
 		}
 		
 	};
 	
 	Answer haniocolor3=new Answer(){
+
+		public void moveWidth(int src,int tag){
+			gv.moveDisk(src, tag);
+			gv.moveDisk(src, tag);
+			gv.moveDisk(src, tag);
+		}
+		
+		public void move(int disks,int a,int b,int c){
+			if(disks==3){
+				moveWidth(a,c);
+			}else{
+				move(disks-3,a,c,b);
+				moveWidth(a,c);
+				move(disks-3,b,a,c);
+			}
+		}
 		
 		@Override
 		public void work() {
-			// TODO Auto-generated method stub
+			for(int i=diskn;i>3;i-=3){
+				move(i-3,1,3,2);
+				gv.moveDisk(1, 3);
+				move(i-3,2,1,3);
+				gv.moveDisk(1, 2);
+				move(i-3,3,1,2);
+				move(i-3,2,3,1);
+			}
+			gv.moveDisk(1, 3);
+			gv.moveDisk(1, 2);
 		}
 		
 	};
@@ -75,4 +127,5 @@ public class HanoiAnswer {
 		ansThread=new Thread(ansRun);
 		ansThread.start();
 	}
+	
 }
